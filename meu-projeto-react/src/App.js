9254805bff36bb4d6e81bde18a0143a11dbf709b
GeorgeMBS <ilustrageorge@gmail.com>
2022-06-23 21:07:11 -0300

    Atualização

--- meu-projeto-react/src/App.js
+++ meu-projeto-react/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import EuMesmo from './components/EuMesmo';
 
 function App() {
   return (
     <div className="App">
       <h1>Olá React!</h1>
       <h2>Meu primeiro APP</h2>
+      <HelloWorld />
+      <EuMesmo/>
     </div>
   )
 }
---
+++ meu-projeto-react/src/components/EuMesmo.js
@@ -0,0 +1,10 @@
+function EuMesmo() {
+    return (
+        <div>
+            <h1>Meu nome é George Michael e...</h1>
+            <p>...estou estudando programação há alguns meses. Confesso que estou gostando muito, sempre fui muito lígado em arte e para mim a criação de sites de certa forma é um meio de se expressar ou ajudar outras pessoas ou empresas á transmitirem mensagens aos seus clientes.</p>
+        </div>
+    )
+}
+
+export default EuMesmo
