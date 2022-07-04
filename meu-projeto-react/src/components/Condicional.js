6622d898afd9b2b95759f8270e5d156fef4f9377
GeorgeMBS <ilustrageorge@gmail.com>
2022-07-03 21:19:04 -0300

    Update Condicional.js

--- meu-projeto-react/src/components/Condicional.js
+++ meu-projeto-react/src/components/Condicional.js
@@ -18,15 +18,25 @@ function Condicional() {
         <div>
             <h2>Cadastre o seu e-mail:</h2>
             <form>
-                <input type="email" placeholder="Digite o seu e-mail..." onChange={(e) => setEmail(e.target.value)}/>
-                <button type="submit" onClick={enviarEmail}>Enviar e-mail</button>
-            {userEmail &&(
+                <input 
+                type="email" 
+                placeholder="Digite o seu e-mail..." 
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <button 
+                type="submit" 
+                onClick={enviarEmail}>
+                    Enviar e-mail
+                    </button>
+                    
+            {userEmail &&( // Aqui abrimos chaves e escrvemos uma condicional "se" em react. dissemos que se o user email for true, ou seja, se ele existir, vamos imprimir na tela esta div com o parágrafo mostrando o email do usuário e o botão para tornar o valor de setUserEmail falsa.
                 <div>
                     <p>O e-mail do usuário é: {userEmail}</p>
                     <button onClick={limparEmail}>Limpar e-mail</button>
                 </div>
             )
             }
+        
             </form>
             
         </div>
