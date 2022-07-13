f64e48fe6ab4449a6d1050edb32a5f4071b6026b
GeorgeMBS <ilustrageorge@gmail.com>
2022-07-12 21:31:03 -0300

    atualização-costs

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import styles from './ProjectForm.module.css'
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
 
-function ProjectForm() {
+function ProjectForm({btnText}) {
+    
+   const [categories, setCategories] = useState([])
+
+   useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+    method:"GET",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+   })
+     .then((resp) => resp.json())
+     .then((data) => {
+        setCategories(data)
+     })
+     .catch((err) => console.log(err))
+   }, [])
+    
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto"/>
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total"/>
-            </div>
-            <div>
-                <select name="category_id">
-                    <option disabled selected>
-                        Selecione a categoria
-                    </option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar projeto" />
-            </div>
+        <form className={styles.form}>
+            <Input 
+                type="text"
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
+            />
+            <Input 
+                type="number"
+                text="Orçamento do projeto"
+                name="Budget"
+                placeholder="Insira o orçamento total"
+            />
+            <Select  
+            name="category_id" 
+            text="Selecione a categoria" 
+            options={categories}
+            />
+            <SubmitButton text={btnText} />
         </form>
  )
 }
