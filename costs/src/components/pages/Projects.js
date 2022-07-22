b3907972baf594c29bc4c3eaa3c2e7f156a32582
GeorgeMBS <ilustrageorge@gmail.com>
2022-07-22 01:03:23 -0300

    atualização da página de projetos.

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,8 +1,29 @@
 import React from "react"
+import { useLocation } from 'react-router-dom'
+import Message from "../layout/Message"
+import styles from './Projects.module.css'
+import Container from '../layout/Container'
+import LinkButton from "../layout/LinkButton"
 
 function Projects() {
-    return (
-    <h1>Projetos</h1>
+
+    const location = useLocation()
+    let message = ''
+    if(location.state) {
+        message = location.state.message
+    }
+
+    return( 
+    <div className={styles.project_container}>
+        <div className={styles.title_container}>
+            <h1>Meus Projetos</h1>
+            <LinkButton to="/newproject" text="Criar Projeto" />
+        </div>
+        {message && <Message type="succes" msg={message}/>}
+        <Container customClass="start">
+            <p>Projetos...</p>
+        </Container>
+    </div>
     )
 }
 
