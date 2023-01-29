cbcde702d7d8813616bd4271ee39d73517e43411
GeorgeMBS <ilustrageorge@gmail.com>
2023-01-29 13:02:51 -0300

    Criação do projeto
    Criação do projeto de formulário multi-step utilizando ReactJS.

---
+++ form_multistep_react/src/App.jsx
@@ -0,0 +1,42 @@
+// components
+
+import {GrFormNext, GrFormPrevious} from 'react-icons/gr'
+import {UserForm} from './components/UserForm'
+import {ReviewForm} from './components/ReviewForm'
+import {Thanks} from './components/Thanks'
+
+import './App.css'
+
+function App() {
+  
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
+
+  return (
+    <div className="App">
+      <div className='header'>
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto
+        </p>
+      </div>
+      <div className='form-container'>
+        <p>etapas</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type='button'>
+              <span>Voltar</span>
+              <GrFormPrevious />
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
